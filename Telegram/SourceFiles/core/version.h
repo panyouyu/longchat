5693b88245a828139afc18470b69e14bc4752db3
ppan2 <111111@qq.com>
2020-11-16 14:59:14 +0800

    version 2.4.0

--- Telegram/SourceFiles/core/version.h
+++ Telegram/SourceFiles/core/version.h
@@ -15,7 +15,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define TDESKTOP_ALPHA_VERSION (0ULL)
 #endif // TDESKTOP_OFFICIAL_TARGET
 
-constexpr auto AppVersion = 2003005;
-constexpr auto AppVersionStr = "2.3.5";
+constexpr auto AppVersion = 2004000;
+constexpr auto AppVersionStr = "2.4.0";
 constexpr auto AppBetaVersion = false;
 constexpr auto AppAlphaVersion = TDESKTOP_ALPHA_VERSION;
